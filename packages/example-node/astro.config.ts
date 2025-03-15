cd94cca362ad83131c0be6da4b8256e61fc4cb4c
Sergey Shishkin <sergey@shishkin.org>
2025-03-15 19:47:56 +0100

    chore: add node example

---
+++ packages/example-node/astro.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
+import pagefind from "astro-pagefind";
+
+export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
+  build: {
+    format: "file",
+  },
+  integrations: [pagefind()],
+});
